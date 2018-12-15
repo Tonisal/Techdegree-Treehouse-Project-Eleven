98411ef134d42ea0934816f30825b5787c5ab5cc
Tonisal <markus.bressel@gmx.de>
2018-12-15 02:44:25 +0100

    add current working tatus

--- src/components/PageNotFound.js
+++
@@ -1,7 +0,0 @@
-//appear when page is not found == url does not match
-import React from 'react';
-
-var PageNotFound = () =>
-  <h1 className="not-found">Page Not Found</h1>;
-
-export default PageNotFound;
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,32 +1,25 @@
-//contains photos received from flickr api
 import React from 'react';
 
-//Components
-import Photo from './Photo';
-
 const Photos = (props) => {
-  /*take data from props*/
-  var result = props.data;
-  let photos;
+    var photos = props.data;
 
-  if (result.length > 0) {
-    /*if array.length more than one(received array), set photos as array*/
-    /*go through each object in array and display each object in photo*/
-    /* pass props --> url of photo and key for id*/
-    photos = result.map( photo =>
-        <li key = {photo.id}><img src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}/></li>
+    if (photos.length > 0) {
+        photos = photos.map(photo =>
+            <li key={photo.id}><img
+                src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}/>
+            </li>
+        );
+    }
+
+    return (
+        <div className="photo-container">
+            <h2>{props.title}</h2>
+            <ul>
+                {photos}
+            </ul>
+        </div>
+
     );
-  }
-
-  return(
-    <div className="photo-container">
-      <h2>{props.title /*take props.title as title*/}</h2>
-      <ul>
-        {photos}
-      </ul>
-    </div>
-
-  );
 }
 
 export default Photos;
