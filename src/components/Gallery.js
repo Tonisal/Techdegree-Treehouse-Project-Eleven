fa8ad278045e30a1608941db1057b834480dae38
Tonisal <markus.bressel@gmx.de>
2018-12-15 02:50:44 +0100

    add current status

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,64 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import apiKey from '../apiKey';
 
-//Components
 import Search from './Search';
 import Photos from './Photos';
 
 class Gallery extends Component {
-  constructor(props) {
-    super();
-    /*Set initial state for photos, url and loading */
-    this.state = {
-      /*Set photos as an empty array*/
-      photos: [],
-      /*Pass search url for any results received*/
-      url: props.url,
-      /*Display loading indicator when array is still being fetched*/
-      loading: true
-    };
-  }
-
-  /*call performSearch*/
-  componentDidMount(){this.performSearch()}
-
-  /*create performSearch and use axios to call flickr api*/
-  /*get flickr array using given apiKey(from config.js) and tag from the props*/
-  performSearch = (tag =this.props.tag) => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=12&format=json&nojsoncallback=1`)
-    .then(response => {
-     /*set photos as array provided, tag as the title and loading to false(for when it's not loading)*/
-      this.setState({
-        photos: response.data.photos.photo,
-        title: tag,
-        loading: false
-      });
-      console.log(response.data.photos.photo);
-      console.log(this.state.title);
-    })
-    .catch(error => {
-      console.log('error',error);
-    });
-  }
-  render() {
-    /*searchBar appear only if url=/search*/
-    let searchBar;
-    if (this.state.url === '/search') {
-      /*Call Search component*/
-      /*onSearch to call on performSearch after input is received*/
-      searchBar = <Search onSearch={this.performSearch} />;
+    constructor(props) {
+        super();
+        this.state = {
+            photos: [],
+            url: props.url,
+        };
     }
 
-    return (
-      <div className="container">
-        {/*call searchBar*/}
-        {searchBar}
-        {/*call photo container and pass data -> photos , title, and loading*/}
-        <Photos data={this.state.photos} title={this.state.title} loading={this.state.loading} />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.SearchForPhotos()
+    }
+
+
+    SearchForPhotos = (tag = this.props.tag) => {
+        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=12&format=json&nojsoncallback=1`)
+            .then(response => {
+                /*set photos as array provided, tag as the title and loading to false(for when it's not loading)*/
+                this.setState({
+                    photos: response.data.photos.photo,
+                    title: tag,
+                });
+            })
+    }
+
+    render() {
+        /*searchBar appear only if url=/search*/
+        let searchBar;
+        if (this.state.url === '/search') {
+            /*Call Search component*/
+            /*onSearch to call on performSearch after input is received*/
+            searchBar = <Search onSearch={this.SearchForPhotos}/>;
+        }
+
+        return (
+            <div className="container">
+                {/*call searchBar*/}
+                {searchBar}
+                {/*call photo container and pass data -> photos , title, and loading*/}
+                <Photos data={this.state.photos} title={this.state.title}/>
+            </div>
+        );
+    }
 }
 
 export default Gallery;
